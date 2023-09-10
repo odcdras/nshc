499aad70aa85b0cfb40ad8cdb961842af7916dc9
ranjeet <ranjeetranjan.raj@gmail.com>
2023-09-10 23:12:24 +0530

    pages added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,7 +18,7 @@ export function Home() {
   return (
     <>
       <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
-        <div className="absolute top-0 h-full w-full bg-[url('./img/bg.jpg')] bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-[url('/img/bg.jpg')] bg-cover bg-center" />
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
         <div className="max-w-8xl container relative mx-auto">
           <div className="flex flex-wrap items-center">
@@ -77,7 +77,7 @@ export function Home() {
                 <CardHeader className="relative h-56">
                   <img
                     alt="Card Image"
-                    src="/src/assets/img/profile.jpg"
+                    src="/img/profile.jpg"
                     className="h-full w-full"
                   />
                 </CardHeader>
